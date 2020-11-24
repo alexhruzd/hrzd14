83af318c827eab2267e6814dd446951a3526ea89
alexhruzd <alex.gaby77@gmail.com>
2020-11-24 12:02:58 +0300

    cleaning project, add leaflet map

---
+++ src/MapComponent.js
@@ -0,0 +1,23 @@
+import React from "react";
+import L from "leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.7.1/dist/images/";
+
+const MapComponent = () => {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
